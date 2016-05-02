05eb9e5d97dca663f36d116acced385858a8cbd0
Ludo <ludovic.smoczynski@gmail.com>
2016-05-02 14:35:40 +0200

    Config prod

--- platforms/ios/www/js/config.js
+++ platforms/ios/www/js/config.js
@@ -1,11 +1,11 @@
 /**
  * les conficufation de l'appliction
  * */
-//var soapurl="http://mobile.ubicentrex.net/";
+var soapurl="http://mobile.ubicentrex.net/";
 //var soapurl="http://copie.ubicentrex.net/";
-var soapurl="http://dev.ubicentrex.net/v154/";
+//var soapurl="http://dev.ubicentrex.net/v154/";
 var debug=true;
 
 //si app version=2.0.3 mettre 203, il faut qu'ici soit la meme dans config global
-var version_app=211;
-var version_app_str="2.1.1";
+var version_app=212;
+var version_app_str="2.1.2";
