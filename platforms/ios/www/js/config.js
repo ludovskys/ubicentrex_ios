030b9f4d68c3ae834f687859df56fd8a6ba945f5
Ludovic Smoczynski <ludovskys@gmail.com>
2017-02-13 17:21:21 +0100

    Version 2.2

--- platforms/ios/www/js/config.js
+++ platforms/ios/www/js/config.js
@@ -1,11 +1,11 @@
 /**
  * configuration de l'appliction
  * */
-//var soapurl="http://mobile.ubicentrex.net/";
+var soapurl="http://mobile.ubicentrex.net/";
 //var soapurl="http://copie.ubicentrex.net/";
-var soapurl="http://dev.ubicentrex.net/v154/";
+//var soapurl="http://dev.ubicentrex.net/v154/";
 var debug=false;
 
 //si app version=2.0.3 mettre 203, il faut qu'ici soit la meme dans config global
-var version_app=217;
-var version_app_str="2.1.7";
+var version_app=220;
+var version_app_str="2.2";
